f7e298e7c90edf0be8f17ab0fcbdaf41b0088cd9
jacobhq <jacob.marshall.jem@gmail.com>
2021-03-02 21:22:12 +0000

    Links in blank

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - start',
-    title: 'start',
+    titleTemplate: '%s - JacobHQ',
+    title: 'Start',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -12,6 +12,9 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
+    base: [
+      { target: '_blank' }
     ]
   },
 
